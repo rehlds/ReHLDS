9071259a32d63286642e052208b043694a3a72fe
BiZaJe <officialgeorg@yandex.ru>
2026-06-10 20:48:35 +0500

    Fixed checks on sv_rehlds_movecmdrate_max_avg

--- rehlds/rehlds/rehlds_security.cpp
+++ rehlds/rehlds/rehlds_security.cpp
@@ -87,7 +87,7 @@ void CMoveCommandRateLimiter::CheckBurstRate(unsigned int clientId) {
 
 void CMoveCommandRateLimiter::CheckAverageRate(unsigned int clientId) {
 	client_t* cl = &g_psvs.clients[clientId];
-	if (!cl->active || sv_rehlds_movecmdrate_max_burst.value <= 0.0f) {
+	if (!cl->active || sv_rehlds_movecmdrate_max_avg.value <= 0.0f) {
 		return;
 	}
 
@@ -168,7 +168,7 @@ void CStringCommandsRateLimiter::CheckBurstRate(unsigned int clientId) {
 
 void CStringCommandsRateLimiter::CheckAverageRate(unsigned int clientId) {
 	client_t* cl = &g_psvs.clients[clientId];
-	if (!cl->active || sv_rehlds_stringcmdrate_max_burst.value <= 0.0f) {
+	if (!cl->active || sv_rehlds_movecmdrate_max_avg.value <= 0.0f) {
 		return;
 	}
 
